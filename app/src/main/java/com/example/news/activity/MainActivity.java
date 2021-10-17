2b58abe0487b3ba6dcb6cb135d3d4597a94abdab
KTarun003 <tarunreddykreddy@gmail.com>
2021-10-17 21:41:46 +0530

    Added News to UI

--- app/src/main/java/com/example/news/activity/MainActivity.java
+++ app/src/main/java/com/example/news/activity/MainActivity.java
@@ -1,42 +1,37 @@
 package com.example.news.activity;
 
-//TODO: * Add a progress bar
-//      * Show "No Internet Connection" message when applicable
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.viewpager2.widget.ViewPager2;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.news.R;
-import com.example.news.adapters.NewsPagerAdapter;
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
 
 
 public class MainActivity extends AppCompatActivity{
-	Toolbar toolbar;
-	TabLayout tabLayout;
-	ViewPager2 viewPager2;
-	NewsPagerAdapter newsPagerAdapter;
-	TabLayoutMediator tabLayoutMediator;
-	String[] tabTitles = new String[]{"HOME","BUSINESS","HEALTH","TECHNOLOGY","SPORTS"};
-
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
 
-		if (!isOnline()) {
+	public boolean notOnline() {
+		ConnectivityManager conMgr = (ConnectivityManager) getApplicationContext()
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo netInfo = conMgr.getActiveNetworkInfo();
+
+		return netInfo == null || !netInfo.isConnected() || !netInfo.isAvailable();
+	}
+
+	public void homeClicked(View view) {
+		if (notOnline()) {
 			try {
 				new AlertDialog.Builder(MainActivity.this)
 						.setTitle("Error")
@@ -48,49 +43,116 @@ public class MainActivity extends AppCompatActivity{
 				Log.d("Main Activity", "Show Dialog: " + e.getMessage());
 			}
 		}
-
-
-	}
-
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		tabLayoutMediator.detach();
-		viewPager2.setAdapter(null);
-	}
-
-	public boolean isOnline() {
-		ConnectivityManager conMgr = (ConnectivityManager) getApplicationContext()
-				.getSystemService(Context.CONNECTIVITY_SERVICE);
-		NetworkInfo netInfo = conMgr.getActiveNetworkInfo();
-
-		if(netInfo == null || !netInfo.isConnected() || !netInfo.isAvailable()){
-			return false;
+		else{
+			Intent intent = new Intent(MainActivity.this, NewsActivity.class);
+			intent.putExtra("type",'D');
+			startActivity(intent);
+			finish();
 		}
-		return true;
-	}
 
-	public void homeClicked(View view) {
-		Toast.makeText(this, "You have clicked Home Category", Toast.LENGTH_LONG).show();
 	}
 
 	public void techClicked(View view) {
-		Toast.makeText(this, "You have clicked Technology Category", Toast.LENGTH_LONG).show();
+		if (notOnline()) {
+			try {
+				new AlertDialog.Builder(MainActivity.this)
+						.setTitle("Error")
+						.setMessage("Internet not available, Please check your internet connectivity and try again later...")
+						.setCancelable(false)
+						.setIcon(android.R.drawable.ic_dialog_alert)
+						.setNeutralButton("OK", (dialog, which) -> finish()).show();
+			} catch (Exception e) {
+				Log.d("Main Activity", "Show Dialog: " + e.getMessage());
+			}
+		}
+		else{
+			Intent intent = new Intent(MainActivity.this, NewsActivity.class);
+			intent.putExtra("type",'T');
+			startActivity(intent);
+			finish();
+		}
 	}
 
-	public void internationalClicked(View view) {
-		Toast.makeText(this, "You have clicked International Category", Toast.LENGTH_LONG).show();
+	public void entertainmentClicked(View view) {
+		if (notOnline()) {
+			try {
+				new AlertDialog.Builder(MainActivity.this)
+						.setTitle("Error")
+						.setMessage("Internet not available, Please check your internet connectivity and try again later...")
+						.setCancelable(false)
+						.setIcon(android.R.drawable.ic_dialog_alert)
+						.setNeutralButton("OK", (dialog, which) -> finish()).show();
+			} catch (Exception e) {
+				Log.d("Main Activity", "Show Dialog: " + e.getMessage());
+			}
+		}
+		else{
+			Intent intent = new Intent(MainActivity.this, NewsActivity.class);
+			intent.putExtra("type",'E');
+			startActivity(intent);
+			finish();
+		}
 	}
 
 	public void sportsClicked(View view) {
-		Toast.makeText(this, "You have clicked Sports Category", Toast.LENGTH_LONG).show();
+		if (notOnline()) {
+			try {
+				new AlertDialog.Builder(MainActivity.this)
+						.setTitle("Error")
+						.setMessage("Internet not available, Please check your internet connectivity and try again later...")
+						.setCancelable(false)
+						.setIcon(android.R.drawable.ic_dialog_alert)
+						.setNeutralButton("OK", (dialog, which) -> finish()).show();
+			} catch (Exception e) {
+				Log.d("Main Activity", "Show Dialog: " + e.getMessage());
+			}
+		}
+		else{
+			Intent intent = new Intent(MainActivity.this, NewsActivity.class);
+			intent.putExtra("type",'S');
+			startActivity(intent);
+			finish();
+		}
 	}
 
 	public void healthClicked(View view) {
-		Toast.makeText(this, "You have clicked Health Category", Toast.LENGTH_LONG).show();
+		if (notOnline()) {
+			try {
+				new AlertDialog.Builder(MainActivity.this)
+						.setTitle("Error")
+						.setMessage("Internet not available, Please check your internet connectivity and try again later...")
+						.setCancelable(false)
+						.setIcon(android.R.drawable.ic_dialog_alert)
+						.setNeutralButton("OK", (dialog, which) -> finish()).show();
+			} catch (Exception e) {
+				Log.d("Main Activity", "Show Dialog: " + e.getMessage());
+			}
+		}
+		else{
+			Intent intent = new Intent(MainActivity.this, NewsActivity.class);
+			intent.putExtra("type",'H');
+			finish();
+		}
 	}
 
 	public void businessClicked(View view) {
-		Toast.makeText(this, "You have clicked Business Category", Toast.LENGTH_LONG).show();
+		if (notOnline()) {
+			try {
+				new AlertDialog.Builder(MainActivity.this)
+						.setTitle("Error")
+						.setMessage("Internet not available, Please check your internet connectivity and try again later...")
+						.setCancelable(false)
+						.setIcon(android.R.drawable.ic_dialog_alert)
+						.setNeutralButton("OK", (dialog, which) -> finish()).show();
+			} catch (Exception e) {
+				Log.d("Main Activity", "Show Dialog: " + e.getMessage());
+			}
+		}
+		else{
+			Intent intent = new Intent(MainActivity.this, NewsActivity.class);
+			intent.putExtra("type",'B');
+			startActivity(intent);
+			finish();
+		}
 	}
 }
---
+++ app/src/main/java/com/example/news/fragments/EntertainmentFragment.java
@@ -0,0 +1,102 @@
+package com.example.news.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.news.R;
+import com.example.news.adapters.NewsAdapter;
+import com.example.news.apiUtilities.NewsArticles;
+import com.example.news.apiUtilities.NewsModelClass;
+import com.example.news.apiUtilities.RetrofitClient;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class EntertainmentFragment extends Fragment {
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		recyclerView.setAdapter(null);
+		progressBar.setVisibility(View.GONE);
+	}
+
+	@Override
+	public void onDetach() {
+		super.onDetach();
+		progressBar.setVisibility(View.INVISIBLE);
+	}
+
+	private List<NewsModelClass> homeNews = new ArrayList<>();
+	String country = "in";
+	String category = "entertainment";
+	int pageSize = 100;
+	private RecyclerView recyclerView;
+	private ProgressBar progressBar;
+	NewsAdapter newsAdapter;
+	final String API_KEY = "ba88d060a3e049ca9fa46f2bea0d52c4";
+
+	public EntertainmentFragment() {
+		// Required empty public constructor
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState)
+	{
+		return inflater.inflate(R.layout.fragment_home, container, false);
+	}
+
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+
+		progressBar = view.findViewById(R.id.pbLoading);
+		progressBar.setVisibility(View.VISIBLE);
+		recyclerView = view.findViewById(R.id.rvHome);
+		recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+		newsAdapter = new NewsAdapter(getContext(),homeNews);
+		recyclerView.setAdapter(newsAdapter);
+		fetchNews();
+	}
+
+	private void fetchNews()
+	{
+		Call<NewsArticles> call = RetrofitClient.getInstance().getMyApi().getNews(country,pageSize,category,API_KEY);
+		call.enqueue(new Callback<NewsArticles>() {
+			@Override
+			public void onResponse(@NonNull Call<NewsArticles> call, @NonNull Response<NewsArticles> response) {
+				if(response.isSuccessful()) {
+					if (!homeNews.isEmpty()) {
+						homeNews.clear();
+					}
+					homeNews.addAll(response.body().getArticles());
+					newsAdapter.UpdateNews(homeNews);
+					progressBar.setVisibility(View.GONE);
+				}
+			}
+
+			@Override
+			public void onFailure(@NonNull Call<NewsArticles> call, Throwable t) {
+				Toast.makeText(getContext(),"Something is wrong",Toast.LENGTH_SHORT).show();
+			}
+		});
+
+
+	}
+}
